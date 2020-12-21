314932348c3595c4714328056e8949238fe481ac
matcool <26722564+matcool@users.noreply.github.com>
2020-12-21 19:44:07 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-red-50 text-lg p-5 w-32 m-5 shadow-lg">Hello :D</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return {};
+    },
+});
+</script>
